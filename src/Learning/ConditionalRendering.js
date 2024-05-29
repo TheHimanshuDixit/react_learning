c35659eb6b520c0a831327605fd6eec8f4c2ead8
Rishabh Dixit <rishabhrajat23sep@gmail.com>
2024-05-29 20:01:29 +0530

    Conditional rendering code

---
+++ src/Learning/ConditionalRendering.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+export default function ConditionalRendering() {
+  const [loggedIn, setLoggedIn] = useState(1);
+  // loggedIn=3
+  // 1,2,3
+  return (
+    <div>
+      {loggedIn === 1 ? (
+        <h1>Welcome User 1</h1>
+      ) : loggedIn === 2 ? (
+        <h1>Welcome User2</h1>
+      ) : (
+        <h1>Welcome User3</h1>
+      )}
+        
+    </div>
+  );
+}
